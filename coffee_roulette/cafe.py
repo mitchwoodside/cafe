e2fd570f1d210699364a5ccba4da29ff7aa7f632
Mitch Woodside <mitchmanwoodside@gmail.com>
2024-01-14 18:19:08 -0500

    improve algorithm

--- coffee_roulette/cafe.py
+++ coffee_roulette/cafe.py
@@ -1,18 +1,19 @@
-import random
 import csv
-import math
 
 import click
 
 from coffee_roulette.match import get_matches
 
+
 def split_list(a):
-    half = len(a)//2
+    half = len(a) // 2
     return a[:half], a[half:]
+
 
 @click.group()
 def cli():
     pass
+
 
 @cli.command()
 @click.option("-l", "--list", "options_file", default="list.csv")
@@ -42,7 +43,7 @@ def spin(options_file, banned_matches_file, update):
 
     for match in matches:
         match = tuple(match)
-        click.echo("{}\t<-->\t{}".format(match[0],match[1]))
+        click.echo("{}\t<-->\t{}".format(match[0], match[1]))
 
     if unmatched_player:
         click.echo("{} went unmatched".format(unmatched_player))
@@ -51,6 +52,7 @@ def spin(options_file, banned_matches_file, update):
         with open(banned_matches_file, "a", newline="") as banned_csv:
             writer = csv.writer(banned_csv)
             writer.writerows(matches)
+
 
 if __name__ == "__main__":
     cli()
--- coffee_roulette/match.py
+++ coffee_roulette/match.py
@@ -1,19 +1,38 @@
 import math
 import random
+from itertools import combinations
+
+import click
+
 
 def split_list(a):
-    half = len(a)//2
+    half = len(a) // 2
     return a[:half], a[half:]
 
-def match_players(names, banned_matches):
-    while True:
-        random.shuffle(names)
-        a,b = split_list(names)
-        matches = {frozenset(i) for i in zip(a, b)}
-        if not matches & banned_matches:
-            break
+
+def match_players(names, available_matches):
+    random.shuffle(names)
+    random.shuffle(available_matches)
+    matches = []
+
+    def valid_match(potential_match, potential_name):
+        other = next(n for n in potential_match if not n == potential_name)
+        return name in potential_match and other in names
+
+    while names:
+        name = names.pop()
+        try:
+            match = next(m for m in available_matches if valid_match(m, name))
+            matches.append(match)
+
+            for player in (p for p in match if not p == name):
+                names.remove(player)
+
+        except StopIteration:
+            raise Exception("Cannot match all players")
 
     return matches
+
 
 def remove_exhausted_players(names, banned_matches):
     matches_exhausted = []
@@ -28,30 +47,44 @@ def remove_exhausted_players(names, banned_matches):
 
     return matches_exhausted
 
+
 def all_matches_banned(names, banned_matches):
-    return len(banned_matches) >= math.comb(len(names),2)
+    return len(banned_matches) >= math.comb(len(names), 2)
+
 
 def ban_self_matches(names, banned_matches):
     for name in names:
-        banned_matches.add(frozenset((name,name)))
+        banned_matches.add(frozenset((name, name)))
 
 
 def get_matches(names, banned_matches):
     unmatched_player = None
+    exhausted_players = []
 
     if not len(names) == len(set(names)):
         raise Exception("not all players are unique")
 
-    exhausted_players = remove_exhausted_players(names, banned_matches)
-    ban_self_matches(names, banned_matches)
+    available_matches = [frozenset(a) for a in combinations(names, 2)]
+    for banned_match in banned_matches:
+        try:
+            available_matches.remove(banned_match)
+        except ValueError:
+            click.echo(f'impossible match banned: {banned_match}')
 
-    if all_matches_banned(names, banned_matches):
+    for name in names:
+        if not any(name in s for s in available_matches):
+            exhausted_players.append(name)
+            names.remove(name)
+
+    if not available_matches:
         raise Exception("All matches are banned")
 
-    if not len(names) == 2 * (len(names)//2):
+    if not len(names) == 2 * (len(names) // 2):
         unmatched_player = random.choice(names)
+        for player_match in (m for m in available_matches if unmatched_player in m):
+            available_matches.remove(player_match)
         names.remove(unmatched_player)
 
-    matches = match_players(names, banned_matches)
+    matches = match_players(names, available_matches)
 
     return matches, unmatched_player, exhausted_players
